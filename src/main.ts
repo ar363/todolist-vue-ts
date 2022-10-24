b600ffa419f4425d40b842d7a1322f5dcf84872e
Aditya Raj <aditya.rajb@gmail.com>
2022-10-24 10:40:30 +0530

    use ts imports for base css + add font + new demo

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,13 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 
+import "normalize.css";
+import "sanitize.css";
+import "sanitize.css/forms.css";
+import "sanitize.css/typography.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 import "./assets/main.css";
 
 const app = createApp(App);
